f66977af9956153c646aa2c602e1c3d564f459f1
Jahmaul Holmes <jahmaulschool@gmail.com>
2020-10-30 13:01:03 -0400

    Add categories to the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,15 @@ class PagesController < ApplicationController
     
     def home
       # redirect_to articles_path if logged_in?
+      @categories = Category.paginate(page: params[:page], per_page: 5)
     end
 
     def about
+    end
+
+    private
+
+    def category_params
+      params.require(:category).permit(:name)
     end
 end

7b021e0f91fa722aa6ea62c2fe247026f2db5901
Jahmaul Holmes <jahmaulschool@gmail.com>
2020-11-01 12:21:15 -0500

    Add image url to new article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password, :imageURL)
   end
 
   def set_user
